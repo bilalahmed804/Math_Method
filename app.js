20afe840dbd0bc7191bba9b8cd5563bce927ec9c
Bilal Ahmed <ba301534@gmail.com>
2024-03-30 00:11:31 +0500

    Math Method update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Chapter 26-30
 // Q.1
-var num = parseInt(prompt("Enter postive integer Number"));
-document.write("Number: " + num + "<br>");
-document.write("Round off value: " + Math.round(num) + "<br>");
-document.write("Floor value: " + Math.floor(num) + "<br>");
-document.write("Ceil value: " + Math.ceil(num) + "<br>")    
+var num_$ = parseInt(prompt("Enter postive integer Number"));
+document.write("Number: " + num_$ + "<br>");
+document.write("Round off value: " + Math.round(num_$) + "<br>");
+document.write("Floor value: " + Math.floor(num_$) + "<br>");
+document.write("Ceil value: " + Math.ceil(num_$) + "<br>")    
