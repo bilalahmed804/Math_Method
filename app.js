389b5a4dde83475a54cc5434a817fffd2e015e66
Bilal Ahmed <ba301534@gmail.com>
2024-03-30 00:05:42 +0500

    Maths Mathod

---
+++ app.js
@@ -0,0 +1,7 @@
+// Chapter 26-30
+// Q.1
+var num = parseInt(prompt("Enter postive integer Number"));
+document.write("Number: " + num + "<br>");
+document.write("Round off value: " + Math.round(num) + "<br>");
+document.write("Floor value: " + Math.floor(num) + "<br>");
+document.write("Ceil value: " + Math.ceil(num) + "<br>")    
